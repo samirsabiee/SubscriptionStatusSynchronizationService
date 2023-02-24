d439b0939c91db5e1f2ee8c656ee2d32fefcf79d
samir <samirsabiee@gmail.com>
2023-02-24 14:09:21 +0330

    add market factory

---
+++ database/factories/MarketFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Market>
+ */
+class MarketFactory extends Factory
+{
+    private $markets = ['GooglePlay', 'AppStore'];
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->randomElement($this->markets)
+        ];
+    }
+}
