927b6dbb8c6d7fc319487077dc59a7eccdb30050
samir <samirsabiee@gmail.com>
2023-02-24 14:08:55 +0330

    optimize Market abstract

--- app/Services/Market.php
+++ app/Services/Market.php
@@ -10,6 +10,11 @@ abstract class Market implements IMarket
         // TODO: Implement getStatus() method.
     }
 
+    public function name(): string
+    {
+        return class_basename(resolve(get_called_class()));
+    }
+
     abstract function appsPlatform(): string;
 
     abstract function config(): array;
