5b17231bd07fe06d3075b70fb71fcc910bd57c16
samir <samirsabiee@gmail.com>
2023-02-24 14:06:27 +0330

    add app seeder

---
+++ database/seeders/AppSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\App;
+use Illuminate\Database\Seeder;
+
+class AppSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        App::factory(10)->create();
+    }
+}
