debd802c2cdc91da8a029d6641fa21ac1b91e7c2
samir <samirsabiee@gmail.com>
2023-02-25 19:10:30 +0330

    retry failed request with 1-hour delay in google play

--- app/Services/Markets/GooglePlay.php
+++ app/Services/Markets/GooglePlay.php
@@ -4,6 +4,7 @@ namespace App\Services\Markets;
 
 use App\Models\Subscription;
 use App\Services\Market;
+use Illuminate\Queue\CallQueuedClosure;
 
 class GooglePlay extends Market
 {
@@ -15,8 +16,10 @@ class GooglePlay extends Market
 
     function updateStatus(Subscription $subscription)
     {
-        if (!$this->fetchSubscriptionStatus('hello')->isSuccessful()) {
-            //TODO
+        if (!$this->fetchSubscriptionStatus($subscription->app->name)->isSuccessful()) {
+            dispatch(CallQueuedClosure::create(function () use ($subscription) {
+                $subscription->market->service->updateStatus($subscription);
+            }))->delay(now()->addHour());
         } else {
             $subscription->update(['status' => $this->getBody()->get('status')]);
         }
