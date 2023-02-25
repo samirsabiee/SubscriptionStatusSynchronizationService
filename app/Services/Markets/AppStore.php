5893f569487693ef539d2f410198d587fdc4a385
samir <samirsabiee@gmail.com>
2023-02-25 19:09:33 +0330

    retry failed request with 2-hour delay for App Store

--- app/Services/Markets/AppStore.php
+++ app/Services/Markets/AppStore.php
@@ -4,6 +4,7 @@ namespace App\Services\Markets;
 
 use App\Models\Subscription;
 use App\Services\Market;
+use Illuminate\Queue\CallQueuedClosure;
 
 class AppStore extends Market
 {
@@ -14,9 +15,11 @@ class AppStore extends Market
 
     function updateStatus(Subscription $subscription)
     {
-        if (!$this->fetchSubscriptionStatus('hello')->isSuccessful()) {
-            //TODO
-        }else {
+        if (!$this->fetchSubscriptionStatus($subscription->app->name)->isSuccessful()) {
+            dispatch(CallQueuedClosure::create(function () use ($subscription) {
+                $subscription->market->service->updateStatus($subscription);
+            }))->delay(now()->addHours(2));
+        } else {
             $subscription->update(['status' => $this->getBody()->get('subscription')]);
         }
     }
