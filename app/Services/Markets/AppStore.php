99c383b1bf9def1e96a41819d3ae0d5a2963e14e
samir <samirsabiee@gmail.com>
2023-02-25 21:12:53 +0330

    pass model insteadof string app name

--- app/Services/Markets/AppStore.php
+++ app/Services/Markets/AppStore.php
@@ -15,7 +15,7 @@ class AppStore extends Market
 
     function updateStatus(Subscription $subscription)
     {
-        if (!$this->fetchSubscriptionStatus($subscription->app->name)->isSuccessful()) {
+        if (!$this->fetchSubscriptionStatus($subscription)->isSuccessful()) {
             dispatch(CallQueuedClosure::create(function () use ($subscription) {
                 $subscription->market->service->updateStatus($subscription);
             }))->delay(now()->addHours(2));
