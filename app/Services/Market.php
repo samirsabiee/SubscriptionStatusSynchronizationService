c7a17dc790e158d2b4af5fa7c2abf8878e0443c8
samir <samirsabiee@gmail.com>
2023-02-25 07:47:17 +0330

    optimize abstract market

--- app/Services/Market.php
+++ app/Services/Market.php
@@ -2,12 +2,33 @@
 
 namespace App\Services;
 
+use App\Models\Subscription;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+
 abstract class Market implements IMarket
 {
+    private $body = null;
+    private $statusCode = null;
 
-    public function getStatus(string $appName)
+    public function fetchSubscriptionStatus(string $appName): self
     {
-        // TODO: Implement getStatus() method.
+        if (is_null($this->body) || is_null($this->statusCode)) {
+            $response = Http::post($this->config()['base_url'], ['app' => $appName]);
+            $this->body = $response->json();
+            $this->statusCode = $response->status();
+        }
+        return $this;
+    }
+
+    public function isSuccessful(): bool
+    {
+        return $this->statusCode == 200;
+    }
+
+    public function getBody(): Collection
+    {
+        return collect($this->body);
     }
 
     public function name(): string
@@ -15,7 +36,7 @@ abstract class Market implements IMarket
         return class_basename(resolve(get_called_class()));
     }
 
-    abstract function appsPlatform(): string;
+    abstract function updateStatus(Subscription $subscription);
 
     abstract function config(): array;
 
