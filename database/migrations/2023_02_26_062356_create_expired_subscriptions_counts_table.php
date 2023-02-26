a6fa629c40ade59cf9080187e15e14733842f20f
samir <samirsabiee@gmail.com>
2023-02-26 07:17:42 +0330

    add table to record run subscription job

---
+++ database/migrations/2023_02_26_062356_create_expired_subscriptions_counts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expired_subscriptions_counts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expired_subscriptions_counts');
+    }
+};
