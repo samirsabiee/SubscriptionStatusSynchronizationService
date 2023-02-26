ea8ecbeb75f020d850ef74337a1dad846b7d0d73
samir <samirsabiee@gmail.com>
2023-02-26 05:55:15 +0330

    add admin user as first user in database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::query()->create([
+            'name' => 'admin',
+            'email' => 'admin@info.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+        ]);
         \App\Models\User::factory(10)->create();
         $this->call(
             [
