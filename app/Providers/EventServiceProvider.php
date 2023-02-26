861a9d9716196ee5d6fe2ed9517022b6155fdd8a
samir <samirsabiee@gmail.com>
2023-02-26 05:55:55 +0330

    register SubscriptionObserver

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Market;
+use App\Models\Subscription;
 use App\Observers\MarketObserver;
+use App\Observers\SubscriptionObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +29,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Market::observe(MarketObserver::class);
+        Subscription::observe(SubscriptionObserver::class);
     }
 
     /**
