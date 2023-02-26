207fcfae12c1ddc3706e5ab83fe320f2127d4145
samir <samirsabiee@gmail.com>
2023-02-26 07:18:19 +0330

    add route for get last expired subscription status

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/googleplay', [\App\Http\Controllers\MarketServiceController::class, 'googlePlay'])->name('market-service.google-play');
 Route::post('/appstore', [\App\Http\Controllers\MarketServiceController::class, 'appstore'])->name('market-service.appstore');
+Route::get('/last/expire/count', [\App\Http\Controllers\LastExpireCountController::class, 'lastExpCount'])->name('last-exp-count');
